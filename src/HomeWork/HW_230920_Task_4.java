e0c068dbfb767c402046971b7f12ecf9de3f8a86
snestsiaronak <snestsiaronak@gmail.com>
2020-09-27 09:25:38 +0300

    hw 23092020

--- src/HomeWork/HW_230920_Task_4.java
+++ src/HomeWork/HW_230920_Task_4.java
@@ -1,7 +1,41 @@
 package HomeWork;
 
-
+import java.util.Arrays;
 
 public class HW_230920_Task_4 {
-   
+    public static void main(String[] args) {
+        //Задача про вектор
+        System.out.println("Массивы!");
+        System.out.println("Одномерные!");
+        System.out.println("Задача про вектор");
+        double[] vector = {21.1, 0.5, 1, -12, 4.8, -14, 18};
+        double minv = 0, maxv = 0, summ = 0, multiply = 1;
+        int min_i = 0, max_i = 0;
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i] < 0) {
+                summ += vector[i];
+            }
+            if (vector[i] > maxv) {
+                maxv = vector[i];
+                max_i = i;
+            }
+            if (vector[i] < minv) {
+                minv = vector[i];
+                min_i = i;
+            }
+        }
+        if (max_i > min_i) {
+            for (int j = min_i + 1; j < max_i; j++) {
+                multiply *= vector[j];
+            }
+        } else {
+            for (int j = max_i + 1; j < min_i; j++) {
+                multiply *= vector[j];
+            }
+        }
+        System.out.println("Вектор!");
+        System.out.println(Arrays.toString(vector));
+        System.out.println("Сумма отрицательных значений = " + summ);
+        System.out.println("Произведение значений между максимальным и минимальным = " + multiply);
+    }
 }
--- src/HomeWork/HW_230920_Task_7.java
+++ src/HomeWork/HW_230920_Task_7.java
@@ -1,6 +1,27 @@
 package HomeWork;
-
+import java.util.Random;
 
 public class HW_230920_Task_7 {
-  
+    public static void main(String[] args) {
+        //Задача о произвольном зубчатом массиве
+        int min = 1;
+        int max = 10;
+        int m1 = 100, m2 = 0;
+        Random random = new Random();
+        System.out.println("Массивы!");
+        System.out.println("Двумерные!");
+        System.out.print("Зубчатый массив произвольный");
+        System.out.println();
+        int rang = random.nextInt(max - min) + min;
+        int[][] arrzub = new int[rang][];
+        for (int i = 0; i < arrzub.length; i++) {
+            arrzub[i] = new int[random.nextInt(m1 - m2) + m2];
+            for (int j = 0; j < arrzub[i].length; j++) {
+                arrzub[i][j] = random.nextInt(m1 - m2) + m2;
+
+                System.out.print(arrzub[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
