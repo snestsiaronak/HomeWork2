9b0bc7a8738cd9e792f26ced2215ac7682129696
snestsiaronak <snestsiaronak@gmail.com>
2020-09-27 19:51:20 +0300

    first hw_changes

--- src/HomeWork/HW_230920_Task_1.java
+++ src/HomeWork/HW_230920_Task_1.java
@@ -5,34 +5,28 @@ import java.util.Scanner;
 public class HW_230920_Task_1 {
     public static void main(String[] args) {
         //Задача про прямоугольник и круг
-        Scanner vvod = new Scanner(System.in);
 
         double a, b, r;
-        //Задача про прямоугольник и круг
         System.out.println("Задача о вписанном прямоугольнике!");
         System.out.print("Введите сторону а: ");
-        while (!vvod.hasNextDouble()) {
-            System.out.print("Это не числло! Попробуй еще раз! ");
-            vvod.next();
-        }
-        a = vvod.nextDouble();
+        a = checkNumber();
         System.out.print("Введите сторону b: ");
-        while (!vvod.hasNextDouble()) {
-            System.out.print("Это не числло! Попробуй еще раз! ");
-            vvod.next();
-        }
-        b = vvod.nextDouble();
+        b = checkNumber();
         System.out.print("Введите радиус r: ");
-        while (!vvod.hasNextDouble()) {
-            System.out.print("Это не числло! Попробуй еще раз! ");
-            vvod.next();
-        }
-        r = vvod.nextDouble();
+        r = checkNumber();
         double diagonal = Math.sqrt(a * a + b * b);
         if (r * 2 >= diagonal) {
             System.out.println("Закрыть можно!");
         } else {
             System.out.println("Закрыть Нельзя!");
         }
+    }
+    static double checkNumber(){
+        Scanner vvod = new Scanner(System.in);
+        while (!vvod.hasNextDouble()) {
+            System.out.print("Это не числло! Попробуй еще раз! ");
+            vvod.next();
+        }
+        return vvod.nextDouble();
     }
 }
